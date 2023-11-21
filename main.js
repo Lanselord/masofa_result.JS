871209e1d32b18f1070c8c4a24b1a0f3cf0d7990
Lanselord <nosirovhumoyiddin23@gmail.com>
2023-11-21 20:17:23 +0500

    JS_yangi

---
+++ main.js
@@ -0,0 +1,35 @@
+var FootSped = 4;
+var BikeSped = 21;
+var CarSped = 70;
+var PlaneSped = 800;
+
+var elForm = document.querySelector(".form");
+var elInput = document.querySelector(".input");
+var elFootresult = document.querySelector(".foot");
+var elBikeresult = document.querySelector(".bike");
+var elCarresult = document.querySelector(".car");
+var elPlaneresult = document.querySelector(".plane");
+
+elForm.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+
+    var Input = elInput.value.trim()
+
+    if(Input) {
+        elFootresult.textContent = Input / FootSped
+    }
+    
+    if(Input) {
+        elBikeresult.textContent = Input / BikeSped
+    }
+    
+    if(Input) {
+        elCarresult.textContent = Input / CarSped
+    }
+    
+    if(Input) {
+        elPlaneresult.textContent = Input / PlaneSped
+    }
+
+    return
+})
